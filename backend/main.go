c809312e12b960f6a686c7116f88a67fc9d78538
Rajeev <rajeevdash07@gmail.com>
2024-01-02 09:30:18 +0530

    added main function

--- backend/main.go
+++ backend/main.go
@@ -16,3 +16,31 @@ type User struct{
 	Name string `json:"name"`
 	Email string `json:"email"`
 }
+
+
+func main(){
+	//connect to the database
+	db, err := sql.Open("postgres".os.Getenv("DATABASE_URL"))
+	if err!= nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	//create table if not exist
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS users (id SERIAL PRIMARY KEY,name TEXT, email TEXT)")
+	if err != nil{
+		log.Fatal(err)
+	} 
+
+	//create router
+	router := mux.NewRouter()
+	router.HandleFunc("/api/go/users",getUsers(db)).Methods("GET")
+	router.HandleFunc("/api/go/users",createUser(db)).Methods("POST")
+	router.HandleFunc("/api/go/users/{id}",getUser(db)).Methods("GET")
+	router.HandleFunc("/api/go/users/{id}",updateUser(db)).Methods("PUT")
+	router.HandleFunc("/api/go/users/{id}",deleteUser(db)).Methods("DELETE")
+
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+	log.Fatal(http.ListenAndServe(":8000",enhancedRouter))
+}

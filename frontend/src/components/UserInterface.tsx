3f7afe4042bd663d6f0d1703820afdca214381f4
RajeevDash007 <ucse21047@stu.xim.edu.in>
2024-01-08 07:20:04 +0530

    updated index.tsx

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -44,6 +44,7 @@ const UserInterface: React.FC<UserInterfaceProps>=({backendName})=>{
 
       return (
         <div className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}>
+          I am good 
         </div>
       );
 };
